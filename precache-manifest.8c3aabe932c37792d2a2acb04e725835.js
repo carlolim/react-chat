d6aef582bd197b93a84d00c74eadb356d93e0309
Carlo James Lim <carlo@sprout.ph>
2018-12-05 20:02:48 +0800

    Updates

---
+++ precache-manifest.8c3aabe932c37792d2a2acb04e725835.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c79d0569fe4b36a81978",
+    "url": "/react-chat/static/js/runtime~main.c79d0569.js"
+  },
+  {
+    "revision": "d3f6fdf3f05359d84e6c",
+    "url": "/react-chat/static/js/main.d3f6fdf3.chunk.js"
+  },
+  {
+    "revision": "44b85a903bab3dc64ddd",
+    "url": "/react-chat/static/js/1.44b85a90.chunk.js"
+  },
+  {
+    "revision": "d3f6fdf3f05359d84e6c",
+    "url": "/react-chat/static/css/main.6a177ab0.chunk.css"
+  },
+  {
+    "revision": "daceb6852412528229307ee4f8dc0ac3",
+    "url": "/react-chat/index.html"
+  }
+];
